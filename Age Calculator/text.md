d3d8fa1e47688953a719e1346f5023c449aedb2c
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-09-23 11:06:52 +0530

    Fifth Project Half Done - Error in API

--- Age Calculator/text.md
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-const yyyy = today.getFullYear();
-const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-const dd = String(today.getDate()).padStart(2, '0');
-
-console.log(`${yyyy}-${mm}-${dd}`); // 2025-09-22
-console.log(`${dd}/${mm}/${yyyy}`); // 22/09/2025
---
+++ Recipe Book App/script.js
@@ -0,0 +1,41 @@
+// fetch('https://dummyjson.com/recipes')
+// .then(res => res.json())
+// // .then(data => console.log(data.recipes[0].ingredients))
+// .then(data => console.log(data.recipes[0].instructions))
+
+fetchRecipe();
+async function fetchRecipe() {
+  let data;
+  try {
+    const response = await fetch("https://dummyjson.com/recipes");
+    if (!response.ok) {
+      throw new Error("Could not fetch the recipes data");
+    }
+    data = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  const recipeImage = document.getElementById("recipe-image");
+  const recipeName = document.getElementById("recipe-name");
+  const recipeIngredients = document.getElementById("recipe-Ingredients");
+  const container = document.querySelector(".container");
+  let recipeCopy;
+  data.recipes.forEach((d) => {
+    recipeCopy += `<div id="recipe-div" class="recipe-class-elem">
+          <div class="image-div">
+            <img id="recipe-image" src=${d.image}/>
+          </div>
+          <h2 id="recipe-name">${d.name}</h2>
+          <span>
+            <p id="recipe-Ingredients">
+             ${d.ingredients}
+            </p>
+            <br><br>
+            <button id="view-recipe-button">View Recipe</button>
+          </span>
+        </div>`;
+  });
+
+  container.innerHTML = recipeCopy;
+}
