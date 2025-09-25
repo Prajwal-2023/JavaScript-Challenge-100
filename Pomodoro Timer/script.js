4536d1833c0f738ef67e16e86ce441254d4d0813
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-09-25 21:22:06 +0530

    Seventh Project Done

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -1,34 +1,55 @@
-let minElem = document.getElementById('min')
-let secElem = document.getElementById('sec')
+let minElem = document.getElementById('min');
+let secElem = document.getElementById('sec');
 
 let minute = 25;
-let seconds = 0 
-minElem.innerText = minute
+let seconds = 0;
+
+minElem.innerText = minute;
 secElem.innerText = seconds.toString().padStart(2, "0");
-const START_BTN = document.getElementById('start-btn')
-const STOP_BTN = document.getElementById('stop-btn')
-const RESET_BTN = document.getElementById('reset-btn')
 
-START_BTN.addEventListener('click',startTimerBtn)
-STOP_BTN.addEventListener('click',stopTimerBtn)
-RESET_BTN.addEventListener('click',resetTimerBtn)
+const START_BTN = document.getElementById('start-btn');
+const STOP_BTN = document.getElementById('stop-btn');
+const RESET_BTN = document.getElementById('reset-btn');
 
-function startTimerBtn(){
-    let strTime = Number(minElem.innerText) 
-    while(strTime > 0){
-        strTime --
+START_BTN.addEventListener('click', startTimerBtn);
+STOP_BTN.addEventListener('click', stopTimerBtn);
+RESET_BTN.addEventListener('click', resetTimerBtn);
+
+let interval = null;
+
+function startTimerBtn() {
+  if (interval !== null) return;
+  interval = setInterval(timer, 1000);
+}
+
+function stopTimerBtn() {
+  clearInterval(interval);
+  interval = null;
+}
+
+function resetTimerBtn() {
+  clearInterval(interval);
+  interval = null;
+  minute = 25;
+  seconds = 0;
+  minElem.innerText = minute;
+  secElem.innerText = seconds.toString().padStart(2, "0");
+}
+
+function timer() {
+  if (seconds === 0) {
+    if (minute === 0) {
+      clearInterval(interval);
+      interval = null;
+      alert("Time's up! Take a break 👏");
+      return;
     }
-    console.log(strTime)
-    
-}
-function stopTimerBtn(){
-    console.log('Stop')
-}
-function resetTimerBtn(){
-    console.log('Reset')
-}
+    minute--;
+    seconds = 59;
+  } else {
+    seconds--;
+  }
 
-function timer(){
-
+  minElem.innerText = minute;
+  secElem.innerText = seconds.toString().padStart(2, "0");
 }
-
