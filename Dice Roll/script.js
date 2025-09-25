b7f23988bd5b779c86ce21b0d10e0288e9f02314
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-09-25 11:31:09 +0530

    Sixth Project Done

--- Dice Roll/script.js
+++ Dice Roll/script.js
@@ -1,10 +1,99 @@
-const diceBtn = document.getElementById('dice-btn')
+const diceBtn = document.getElementById("dice-btn");
+const diceShapeElem = document.querySelector(".actual-dice");
 
-function randomDiceFace(){
-    const diceface = Math.floor((Math.random() * 6) + 1)
-    console.log(diceface)
+function randomDiceFace() {
+  diceShapeElem.innerHTML = "";
+  diceShapeElem.style.transition = "transform 0.5s ease";
+  diceShapeElem.style.transform = `rotate(360deg)`;
+  setTimeout(() => {
+    diceShapeElem.style.transform = "rotate(0deg) translateX(0) translateY(0)";
+  }, 500);
+
+  const diceface = Math.floor(Math.random() * 6 + 1);
+  console.log(diceface);
+
+  switch (diceface) {
+    case 1:
+      setTimeout(() => {
+        diceShapeElem.innerHTML += `
+          <div class="dice-position"></div>`;
+      }, 600);
+      break;
+    case 2:
+      setTimeout(() => {
+        diceShapeElem.innerHTML += `
+            <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+          </div>`;
+      }, 600);
+      break;
+    case 3:
+      setTimeout(() => {
+        diceShapeElem.innerHTML += `
+       <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div></div>
+          </div>
+          <div>
+            <div class="dice-position"></div>
+          </div>
+          <div id="shape-parent">
+            <div></div>
+            <div class="dice-position"></div>
+          </div>
+      `;
+      }, 600);
+      break;
+    case 4:
+      setTimeout(() => {
+        diceShapeElem.innerHTML += `
+           <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+          </div>
+          <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+          </div>
+      `;
+      }, 600);
+
+      break;
+    case 5:
+      setTimeout(() => {
+        diceShapeElem.innerHTML += `
+           <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+          </div>
+          <div>
+            <div class="dice-position"></div>
+          </div>
+          <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+          </div>`;
+      }, 600);
+      break;
+    case 6:
+      setTimeout(() => {
+        diceShapeElem.innerHTML += `
+           <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+            <div class="dice-position"></div> 
+          </div>
+          <div id="shape-parent">
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+            <div class="dice-position"></div>
+          </div>`;
+      }, 600);
+      break;
+  }
 }
 
-diceBtn.addEventListener('click',()=>{
-    randomDiceFace()
-})
+diceBtn.addEventListener("click", () => {
+  randomDiceFace();
+});
