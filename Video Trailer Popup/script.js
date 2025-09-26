a853a95ca8e8196d14c7cd841516ed68dc0c32a4
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-09-26 15:44:29 +0530

    Video Trailer Project Done

--- Video Trailer Popup/script.js
+++ Video Trailer Popup/script.js
@@ -1,10 +1,26 @@
-const mainImage = document.querySelector('.main img');
-const watchButton = document.querySelector('.details-div button');
+const mainImage = document.querySelector(".main img");
+const watchButton = document.querySelector(".details-div button");
+const videoParent = document.querySelector(".video-parent");
+const videoParentVideo = document.querySelector(".video-parent video");
+const closeVideoParent = document.querySelector(".video-parent span");
 
+mainImage.addEventListener("click", () => {
+  targetVideoPrevFnc();
+});
+watchButton.addEventListener("click", () => {
+  targetVideoPrevFnc();
+});
 
-mainImage.addEventListener('click',()=>{
-    alert('ok')
-})
-watchButton.addEventListener('click',()=>{
-    alert('Button')
-})
+function targetVideoPrevFnc() {
+  videoParent.style.display = "flex";
+  videoParentVideo.muted = true;
+  videoParentVideo.autoplay = true;
+  videoParentVideo.play();
+}
+
+function closeVideoParentFnc() {
+  closeVideoParent.addEventListener("click", () => {
+    videoParent.style.display = "none";
+  });
+}
+closeVideoParentFnc();
