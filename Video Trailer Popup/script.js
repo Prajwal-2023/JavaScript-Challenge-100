e581e8b815c40ac8cd761ff0707a0a15d18ccfa1
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-09-26 15:24:56 +0530

    Video Trailer Project Half Done

---
+++ Video Trailer Popup/script.js
@@ -0,0 +1,10 @@
+const mainImage = document.querySelector('.main img');
+const watchButton = document.querySelector('.details-div button');
+
+
+mainImage.addEventListener('click',()=>{
+    alert('ok')
+})
+watchButton.addEventListener('click',()=>{
+    alert('Button')
+})
